8f41c4dbd2aca0f1b5d8d6e9b74812007bacc18d
tushardubey <dubey.tushar23@gmail.com>
2021-01-14 19:46:18 +0530

    super added

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,14 +1,22 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
 
   final count = 0.obs;
   @override
-  void onInit() {}
+  void onInit() {
+    super.onInit();
+  }
+
   @override
-  void onReady() {}
+  void onReady() {
+    super.onReady();
+  }
+
   @override
-  void onClose() {}
+  void onClose() {
+    super.onClose();
+  }
+
   void increment() => count.value++;
 }
