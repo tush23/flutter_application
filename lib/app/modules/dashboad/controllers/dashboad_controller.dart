85599daa0fa704d68db4f3d450c5efcb22ddd7f5
tushardubey <dubey.tushar23@gmail.com>
2021-01-17 18:42:22 +0530

    ad

--- lib/app/modules/dashboad/controllers/dashboad_controller.dart
+++ lib/app/modules/dashboad/controllers/dashboad_controller.dart
@@ -2,11 +2,11 @@ import 'package:get/get.dart';
 
 class DashboadController extends GetxController {
   final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
-  @override
-  void onClose() {}
+  // @override
+  // void onInit() {}
+  // @override
+  // void onReady() {}
+  // @override
+  // void onClose() {}
   void increment() => count.value++;
 }
