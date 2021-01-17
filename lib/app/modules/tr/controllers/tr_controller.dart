250226c403c1c12c80e8ecbbea8f5dd358b448f3
tushardubey <dubey.tushar23@gmail.com>
2021-01-17 18:40:13 +0530

    added icons

--- lib/app/modules/tr/controllers/tr_controller.dart
+++ lib/app/modules/tr/controllers/tr_controller.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 
 class TrController extends GetxController {
-  //TODO: Implement TrController
 
   final count = 0.obs;
   @override
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_for_git_profile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
