0d8fac17dd70763fb746d36a12091019591583bd
tushardubey <dubey.tushar23@gmail.com>
2021-01-18 23:12:25 +0530

    test added

--- lib/app/modules/store/controllers/store_controller.dart
+++ lib/app/modules/store/controllers/store_controller.dart
@@ -4,8 +4,7 @@ class StoreController extends GetxController {
   //TODO: Implement StoreController
 
   final count = 0.obs;
-  @override
-  void onInit() {}
+
 
   void increment() => count.value++;
 }
