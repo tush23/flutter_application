9def94da2369174a6bba7f0b767ecb5f04484fcd
tushardubey <dubey.tushar23@gmail.com>
2021-01-18 23:12:58 +0530

    test for home

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,14 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_for_git_profile/app/modules/home/views/home_view.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(HomeP());
+    await tester.pumpWidget(HomeView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
