c76f74bfe2e58dfc6525bb7bf022e45b6f10eeb6
tushardubey <dubey.tushar23@gmail.com>
2021-01-18 23:11:11 +0530

    changes

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -2,14 +2,5 @@ import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
 
-  final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
-  @override
-  void onClose() {}
-  void increment() => count.value++;
-
   
 }
