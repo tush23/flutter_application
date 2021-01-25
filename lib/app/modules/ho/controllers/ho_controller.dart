c0d6b7f53eb8c20412be867a37359030fb00bab3
tushardubey <dubey.tushar23@gmail.com>
2021-01-25 12:27:51 +0530

    super

--- lib/app/modules/ho/controllers/ho_controller.dart
+++ lib/app/modules/ho/controllers/ho_controller.dart
@@ -3,7 +3,10 @@ import 'package:get/get.dart';
 class HoController extends GetxController {
   final count = 0.obs;
   @override
-  void onClose() {}
+  void onClose() {
+    super.onClose();
+  }
+
   void increment() => count.value++;
 }
 //tushar dubey
