3c5ee5bec8d5bd85626862b8834c0e90eec69f1b
tushardubey <dubey.tushar23@gmail.com>
2021-01-25 12:09:29 +0530

    addedd

--- lib/app/modules/dashboad/bindings/dashboad_binding.dart
+++ lib/app/modules/dashboad/bindings/dashboad_binding.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-
 import 'package:flutter_application_for_git_profile/app/modules/dashboad/controllers/dashboad_controller.dart';
 
 class DashboadBinding extends Bindings {
