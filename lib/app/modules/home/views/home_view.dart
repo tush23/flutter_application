467226a20f763b91c6d494c6510aee51cea6608d
tushardubey <dubey.tushar23@gmail.com>
2021-01-27 13:52:39 +0530

    sm changes

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,6 +7,7 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: Text('HomeView'),
         centerTitle: true,
       ),
