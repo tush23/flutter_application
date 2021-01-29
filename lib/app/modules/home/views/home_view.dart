18ae8965dadcfe93dd3421adbeb504d40f37dfd5
tushardubey <dubey.tushar23@gmail.com>
2021-01-29 10:47:59 +0530

    sp

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:flutter_application_for_git_profile/app/modules/home/controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
