8f5dc5382abf96e7aaa72a553857f896813061c0
tushardubey <dubey.tushar23@gmail.com>
2021-01-29 10:48:44 +0530

    asfa

--- lib/app/modules/dashboad/views/dashboad_view.dart
+++ lib/app/modules/dashboad/views/dashboad_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
 import 'package:flutter_application_for_git_profile/app/modules/dashboad/controllers/dashboad_controller.dart';
 
 class DashboadView extends GetView<DashboadController> {
