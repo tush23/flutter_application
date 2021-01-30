86dbc4eb79ade1b298640eee248166a7bdccff67
tushardubey <dubey.tushar23@gmail.com>
2021-01-30 17:01:27 +0530

    spaces removed1.4

--- lib/app/modules/dashboad/views/dashboad_view.dart
+++ lib/app/modules/dashboad/views/dashboad_view.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_application_for_git_profile/app/modules/dashboad/controllers/dashboad_controller.dart';
 
-
 class DashboadView extends GetView<DashboadController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-    
+        
         title: Text('DashboadView'),
 
         centerTitle: true,
