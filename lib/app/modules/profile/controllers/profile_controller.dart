260cb10451fe589b4d2df7a5b7de465a2287b2fa
tushardubey <dubey.tushar23@gmail.com>
2021-02-02 11:50:48 +0530

    onready removed controller

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -2,11 +2,5 @@ import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
 
-  final count = 0.obs;
-  @override
-  void onInit() {}
-
-  void increment() => count.value++;
-
   
 }
