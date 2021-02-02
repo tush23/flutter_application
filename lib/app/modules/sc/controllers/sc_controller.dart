7d1fb78f510c32bab7d2c5382937ce82bdb9413d
tushardubey <dubey.tushar23@gmail.com>
2021-02-02 11:51:14 +0530

    onready removed

--- lib/app/modules/sc/controllers/sc_controller.dart
+++ lib/app/modules/sc/controllers/sc_controller.dart
@@ -1,14 +1,3 @@
 import 'package:get/get.dart';
 
-class ScController extends GetxController {
-  //TODO: Implement ScController
-
-  final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
-  @override
-  void onClose() {}
-  void increment() => count.value++;
-}
+class ScController extends GetxController {}
