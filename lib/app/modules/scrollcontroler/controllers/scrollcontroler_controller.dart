9d97245e716729eabf48664bc5027696207b0f53
tushardubey <dubey.tushar23@gmail.com>
2021-02-02 11:52:11 +0530

    unwanted code removal

--- lib/app/modules/scrollcontroler/controllers/scrollcontroler_controller.dart
+++ lib/app/modules/scrollcontroler/controllers/scrollcontroler_controller.dart
@@ -1,14 +1,3 @@
 import 'package:get/get.dart';
 
-class ScrollcontrolerController extends GetxController {
-  //TODO: Implement ScrollcontrolerController
-
-  final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
-  @override
-  void onClose() {}
-  void increment() => count.value++;
-}
+class ScrollcontrolerController extends GetxController {}
--- lib/app/modules/store/controllers/store_controller.dart
+++ lib/app/modules/store/controllers/store_controller.dart
@@ -1,10 +1,4 @@
 import 'package:get/get.dart';
 
 class StoreController extends GetxController {
-  //TODO: Implement StoreController
-
-  final count = 0.obs;
-
-
-  void increment() => count.value++;
 }
