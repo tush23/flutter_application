1cc62962b053505c8da46982fac8254b998b4f6e
tushardubey <dubey.tushar23@gmail.com>
2021-02-07 15:42:05 +0530

    smal changes done

--- lib/app/modules/dash/controllers/dash_controller.dart
+++ lib/app/modules/dash/controllers/dash_controller.dart
@@ -4,10 +4,7 @@ class DashController extends GetxController {
   //TODO: Implement DashController
 
   final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
+ 
   @override
   void onClose() {}
   void increment() => count.value++;
